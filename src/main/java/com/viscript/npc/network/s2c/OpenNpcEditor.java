3cdb4a06bbad8d06401a05b299b56df4b2f9222d
zhenshiz <2238446247@qq.com>
2025-11-08 00:02:45 +0800

    修复一个小bug（非开发环境下IScreenTest不存在所以直接new就行了），优化gradle结构和前置关系

--- src/main/java/com/viscript/npc/network/s2c/OpenNpcEditor.java
+++ src/main/java/com/viscript/npc/network/s2c/OpenNpcEditor.java
@@ -1,13 +1,10 @@
 package com.viscript.npc.network.s2c;
 
-import com.lowdragmc.lowdraglib2.LDLib2Registries;
 import com.lowdragmc.lowdraglib2.Platform;
 import com.lowdragmc.lowdraglib2.gui.ui.ModularUI;
 import com.lowdragmc.lowdraglib2.gui.ui.ModularUIScreen;
-import com.lowdragmc.lowdraglib2.registry.AutoRegistry;
-import com.lowdragmc.lowdraglib2.registry.annotation.LDLRegisterClient;
-import com.lowdragmc.lowdraglib2.test.ui.IScreenTest;
 import com.viscript.npc.ViScriptNpc;
+import com.viscript.npc.test.TestNpcEditor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
@@ -15,8 +12,6 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.function.Supplier;
 
 public record OpenNpcEditor() implements CustomPacketPayload {
     public static final Type<OpenNpcEditor> TYPE = new Type<>(ViScriptNpc.id("open_npc_editor"));
@@ -31,12 +26,10 @@ public record OpenNpcEditor() implements CustomPacketPayload {
 
     public static void execute(OpenNpcEditor payload, IPayloadContext context) {
         Minecraft minecraft = Minecraft.getInstance();
-        AutoRegistry.Holder<LDLRegisterClient, IScreenTest, Supplier<IScreenTest>> npcEditor = LDLib2Registries.SCREEN_TESTS.get("npc_editor");
-        if (npcEditor != null) {
-            ModularUI ui = npcEditor.value().get().createUI(minecraft.player);
-            if (!Platform.isDevEnv()) ui.shouldCloseOnEsc(false).shouldCloseOnKeyInventory(false);
-            minecraft.setScreen(new ModularUIScreen(ui, Component.empty()));
-        }
+        TestNpcEditor testNpcEditor = new TestNpcEditor();
+        ModularUI ui = testNpcEditor.createUI(minecraft.player);
+        if (!Platform.isDevEnv()) ui.shouldCloseOnEsc(false).shouldCloseOnKeyInventory(false);
+        minecraft.setScreen(new ModularUIScreen(ui, Component.empty()));
     }
 
     @Override
